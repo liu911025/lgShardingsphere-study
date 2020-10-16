38b42246a4f0a96031a2b7a89e5f4927226ddab5
ljt <329311024@qq.com>
2020-10-16 10:56:47 +0800

    数据脱敏, 明文列没有存储

--- sharding-springboot/src/main/java/com/study/sharding/controller/UserController.java
+++ sharding-springboot/src/main/java/com/study/sharding/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.study.sharding.controller;
 
 import com.study.sharding.pojo.User;
+import com.study.sharding.service.EncryptUserService;
 import com.study.sharding.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,23 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private EncryptUserService encryptUserService;
 
     @RequestMapping("addUser")
     public String initUser() {
         try {
             userService.processUsers();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "success";
+    }
+
+    @RequestMapping("addEncryptUser")
+    public String addEncryptUser() {
+        try {
+            encryptUserService.processEncryptUsers();
         } catch (SQLException e) {
             e.printStackTrace();
         }
